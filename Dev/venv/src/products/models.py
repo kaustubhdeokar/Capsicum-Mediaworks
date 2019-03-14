e090c8792fec3a3387cb7520a563cc1ddb2943c9
kaustubhdeokar <kaustubhdcpo@gmail.com>
2019-03-14 22:49:56 +0530

    submit-to-db

--- Dev/venv/src/products/models.py
+++ Dev/venv/src/products/models.py
@@ -5,8 +5,8 @@ class Product(models.Model):
 	title	    = models.CharField(max_length=120) 
 	description = models.TextField(blank=True, null=True)
 	price	= models.DecimalField(decimal_places=2,max_digits=1000)
-	summary     = models.TextField()
-	featured = models.BooleanField()
+	summary     = models.TextField(blank=False, null=False)
+	featured = models.BooleanField(default=False)
 	
 
 
--- Dev/venv/src/products/views.py
+++ Dev/venv/src/products/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
+from .forms import ProductForm
 from .models import Product
+
+def product_create_view(request):
+	form = ProductForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+		form = ProductForm()
+	context = {
+		'form':form
+	}
+	return render(request,"products/product_create.html",context)
+
+
+
 # Create your views here.
 def product_detail_view(request):
 	obj=Product.objects.get(id=1)
 	context={
 		'object':obj
 	}
-	return render(request,"product/detail.html",context)
+	return render(request,"products/product_detail.html",context)
+#here the template is inside the app products.
