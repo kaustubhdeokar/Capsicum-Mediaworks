e1b9173b815f695dfd8eb12e54c49997119467c2
kaustubhdeokar <kaustubhdcpo@gmail.com>
2019-03-14 18:08:23 +0530

    db rendered

--- Dev/venv/src/projectone/urls.py
+++ Dev/venv/src/projectone/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import home_view,contact_view,about_view
+from products.views import product_detail_view
 
 urlpatterns = [
+    path('product/',product_detail_view),
 	path('about/',about_view),
 	path('contact/',contact_view),
 	path('',home_view,name='home'),
