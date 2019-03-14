8b40ab6ca540609126e75ab86aec4934a64f4bec
kaustubhdeokar <kaustubhdcpo@gmail.com>
2019-03-14 15:59:40 +0530

    rendering to templates

--- Dev/venv/src/pages/views.py
+++ Dev/venv/src/pages/views.py
@@ -9,3 +9,11 @@ def home_view(request,*args,**kwargs):
 def contact_view(request,*args,**kwargs):
 	#return HttpResponse("<h1>Contacts Page</h1>")
 	return render(request,"contacts.html",{})
+
+def about_view(request,*args,**kwargs):
+	my_context={
+	"my_text":"This is about us",
+	"my_number":123,
+	"my_list":[1,2,3]
+	}
+	return render(request,"about.html",my_context)
--- Dev/venv/src/projectone/urls.py
+++ Dev/venv/src/projectone/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pages.views import home_view,contact_view
+from pages.views import home_view,contact_view,about_view
 
 urlpatterns = [
+	path('about/',about_view),
 	path('contact/',contact_view),
 	path('',home_view,name='home'),
     path('admin/', admin.site.urls),
