a2ca11495f0e7669d5e1c48ee22f9c5a5b4abea9
kaustubhdeokar <kaustubhdcpo@gmail.com>
2019-03-14 10:49:57 +0530

    template inheritance

--- Dev/venv/src/pages/views.py
+++ Dev/venv/src/pages/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request,*args,**kwargs):
-	return render(request,"ques1_1.html",{})
+	return render(request,"home.html",{})
 	#return HttpResponse("<h1>Hello World</h1>") #html code-string form
 
 def contact_view(request,*args,**kwargs):
-	return HttpResponse("<h1>Contacts Page</h1>")
-	print(request.user)
+	#return HttpResponse("<h1>Contacts Page</h1>")
+	return render(request,"contacts.html",{})
