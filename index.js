41bc7824b7abf6c311f8fa4012fba826dae9b586
kaustubhdeokar <kaustubhd9@gmail.com>
2019-06-04 23:29:55 +0530

    dev1

--- index.js
+++ index.js
@@ -4,25 +4,7 @@ const path=require('path');
 const fs=require('fs');
 
 const server=http.createServer((req,res)=>{
-    if(req.url === "/"){
-        
-        fs.readFile(path.join(__dirname,'public','index.html'),(err,content)=>{
-            if(err)throw err;
-            res.writeHead(200,{"Content-type":'text/html'});
-            res.end(content);    
-        });
-    }
-
-    if(req.url === "/about"){
-        
-        fs.readFile(path.join(__dirname,'public','about.html'),(err,content)=>{
-            if(err)throw err;
-            res.writeHead(200,{"Content-type":'text/html'});
-            res.end(content);    
-        });
-    }
-    
-
+  
 });
 
 
